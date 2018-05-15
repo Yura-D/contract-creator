1fa7de6eb998b3d1f57674e874b3d149bd559f3d
Yura-D <dyachuk.yu@gmail.com>
2018-05-15 12:23:45 +0300

    add serch and replace like def

--- document.py
+++ document.py
@@ -1,19 +1,16 @@
 from docx import Document
 from docx.shared import Inches
 
+def text_replace(old_text, new_text):
+    doc = Document("test.docx")
+    for p in doc.paragraphs:
+        if old_text in p.text:
+            inline = p.runs
+            # Loop added to work with runs (strings with same style)
+            for i in range(len(inline)):
+                if old_text in inline[i].text:
+                    text = inline[i].text.replace(old_text, new_text)
+                    inline[i].text = text
+    doc.save('dest1.docx')
 
-#document = Document("test.docx")
-Director = "Дячук Юрій Ігорович"
-
-doc = Document("test.docx")
-for p in doc.paragraphs:
-    if 'Директора' in p.text:
-        inline = p.runs
-        # Loop added to work with runs (strings with same style)
-        for i in range(len(inline)):
-            if 'Директора' in inline[i].text:
-                text = inline[i].text.replace('Директора', Director)
-                inline[i].text = text
-        print (p.text)
-
-doc.save('dest1.docx')
+text_replace("Директора", "Дячук Юрій Ігорович")
