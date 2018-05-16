f6c535b96aa782150c58013bc27612b85abb5755
Yura-D <dyachuk.yu@gmail.com>
2018-05-16 15:40:48 +0300

    add requirements file, change something in the search-replace function, make posible to overwrite file and create new file with date in the name. Also add txt file with names to serching and replacing for template.

--- document.py
+++ document.py
@@ -1,16 +1,26 @@
 from docx import Document
 from docx.shared import Inches
 
-def text_replace(old_text, new_text):
-    doc = Document("test.docx")
+
+template = "test.docx"
+
+contract_date = input("Please type the date of contract (Example - 08.05.2018):\n")
+
+file_name_date = contract_date[6:10] + contract_date[2:6] + contract_date[0:2]
+new_file = file_name_date + "-" + str(template)
+
+def text_replace(old_text, new_text, file):
+    doc = Document(file)
     for p in doc.paragraphs:
         if old_text in p.text:
             inline = p.runs
             # Loop added to work with runs (strings with same style)
-            for i in range(len(inline)):
-                if old_text in inline[i].text:
-                    text = inline[i].text.replace(old_text, new_text)
-                    inline[i].text = text
-    doc.save('dest1.docx')
+            for line in inline:
+                if old_text in line.text:
+                    text = line.text.replace(old_text, new_text)
+                    line.text = text
+    doc.save(new_file)
 
-text_replace("Директора", "Дячук Юрій Ігорович")
+text_replace("Директора", "Дячук Юрій Ігорович", template)
+text_replace("Petro", "Іван", new_file)
+text_replace("dfdfdj", "Hello world", new_file)
