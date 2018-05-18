1bdebab6318d6a602b627449b8debf31dd438bd5
Yura-D <dyachuk.yu@gmail.com>
2018-05-18 22:48:30 +0300

    make function to convert digital date into month by words better

--- document.py
+++ document.py
@@ -10,48 +10,40 @@ file_name_date = contract_date[6:10] + contract_date[2:6] + contract_date[0:2]
 new_file = file_name_date + "-" + str(template)
 #to create the file name with revers position of date
 
-month_by_word = str()
-if contract_date[3:5] == "01":
-    month_by_word = "січня"
-elif contract_date[3:5] == "02":
-    month_by_word = "лютого"
-elif contract_date[3:5] == "03":
-    month_by_word = "березня"
-elif contract_date[3:5] == "04":
-    month_by_word = "квітня"
-elif contract_date[3:5] == "05":
-    month_by_word = "травня"
-elif contract_date[3:5] == "06":
-    month_by_word = "червня"
-elif contract_date[3:5] == "07":
-    month_by_word = "липня"
-elif contract_date[3:5] == "08":
-    month_by_word = "серпня"
-elif contract_date[3:5] == "09":
-    month_by_word = "вересня"
-elif contract_date[3:5] == "10":
-    month_by_word = "жовтня"
-elif contract_date[3:5] == "11":
-    month_by_word = "листопада"
-elif contract_date[3:5] == "12":
-    month_by_word = "грудня"
+def get_month(contract_date):
+    return {
+        "01": "січня",
+        "02": "лютого",
+        "03": "березня",
+        "04": "квітня",
+        "05": "травня",
+        "06": "червня",
+        "07": "липня",
+        "08": "серпня",
+        "09": "вересня",
+        "10": "жовтня",
+        "11": "листопада",
+        "12": "грудня"
+    }.get(contract_date[3:5])
+
+month_by_word = get_month(contract_date)
 
 date_by_word = contract_date[0:2] + " " + month_by_word + " " + contract_date[6:10]
 #date by word for replacing date instide in contract
 
-c_name = "1"
-c_passport_id = "2"
-c_passport_address = "3"
-c_passport_date = "4"
-c_address = "5"
-c_fop_address = "6"
-c_id = "7"
-c_bank = "8"
-c_bank_info = "9"
-c_person_bank_info = "10"
-c_fop_id = "11"
-c_fop_id_date = "12"
-c_born = "13"
+c_name = "test1"
+c_passport_id = "test2"
+c_passport_address = "test3"
+c_passport_date = "test4"
+c_address = "test5"
+c_fop_address = "test6"
+c_id = "test7"
+c_bank = "test8"
+c_bank_info = "test9"
+c_person_bank_info = "test10"
+c_fop_id = "test11"
+c_fop_id_date = "test12"
+c_born = "test13"
 #need for future functional. Now only like test
 
 def text_replace(old_text, new_text, file):
@@ -81,3 +73,4 @@ text_replace("${FOP ID}", c_fop_id, new_file)
 text_replace("${FOP ID date}", c_fop_id_date, new_file)
 text_replace("${Born}", c_born, new_file)
 #to replace everything that you need in the template
+
