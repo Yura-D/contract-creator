a25dff0f431d51a5860e97760f34d66d33f4d9a3
Yura-D <dyachuk.yu@gmail.com>
2018-05-22 04:01:48 +0300

    make working with Google sheet, searching of employee and autocompleting templates

--- document.py
+++ document.py
@@ -1,15 +1,27 @@
 from docx import Document
 from docx.shared import Inches
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 
-template = "test.docx"
+
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
+client = gspread.authorize(creds)
+
+sheet = client.open("employee's info").sheet1
+
+search = input("Find the emploee (enter full name): ")
+
+
+unit = sheet.findall(search)
+unit = str(unit)
+row_unit = unit.split("[<Cell R")
+row_unit = row_unit[1].split("C")
+row_number = row_unit[0]
+# make better pursing of row number
 
 contract_date = input("Please type the date of contract (Example - 08.05.2018):\n")
-
-file_name_date = contract_date[6:10] + contract_date[2:6] + contract_date[0:2]
-new_file = file_name_date + "-" + str(template)
-# to create the file name with revers position of date
-
 
 def get_month(contract_date):
     return {
@@ -33,20 +45,19 @@ month_by_word = get_month(contract_date)
 date_by_word = contract_date[0:2] + " " + month_by_word + " " + contract_date[6:10]
 # date by word for replacing date instide in contract
 
-c_name = "test1"
-c_passport_id = "test2"
-c_passport_address = "test3"
-c_passport_date = "test4"
-c_address = "test5"
-c_fop_address = "test6"
-c_id = "test7"
-c_bank = "test8"
-c_bank_info = "test9"
-c_person_bank_info = "test10"
-c_fop_id = "test11"
-c_fop_id_date = "test12"
-c_born = "test13"
-# need for future functional. Now only like test
+c_name = sheet.cell(row_number, 2).value
+c_passport_id = sheet.cell(row_number, 4).value
+c_passport_address = sheet.cell(row_number, 5).value
+c_passport_date = sheet.cell(row_number, 7).value
+c_address = sheet.cell(row_number, 8).value
+c_fop_address = sheet.cell(row_number, 9).value
+c_id = sheet.cell(row_number, 11).value
+c_bank = sheet.cell(row_number, 12).value
+c_bank_info = sheet.cell(row_number, 14).value
+c_person_bank_info = sheet.cell(row_number, 15).value
+c_fop_id = sheet.cell(row_number, 16).value
+c_fop_id_date = sheet.cell(row_number, 17).value
+c_born = sheet.cell(row_number, 21).value
 
 
 def text_replace(old_text, new_text, file):
@@ -62,18 +73,29 @@ def text_replace(old_text, new_text, file):
     doc.save(new_file)
 
 
-text_replace("${Contract date}", date_by_word, template)
-text_replace("${Name}", c_name, new_file)
-text_replace("${Passport ID}", c_passport_id, new_file)
-text_replace("${Passport address}", c_passport_address, new_file)
-text_replace("${Passport date}", c_passport_date, new_file)
-text_replace("${Address}", c_address, new_file)
-text_replace("${FOP address}", c_fop_address, new_file)
-text_replace("${ID}", c_id, new_file)
-text_replace("${Bank}", c_bank, new_file)
-text_replace("${Bank info}", c_bank_info, new_file)
-text_replace("${Person bank info}", c_person_bank_info, new_file)
-text_replace("${FOP ID}", c_fop_id, new_file)
-text_replace("${FOP ID date}", c_fop_id_date, new_file)
-text_replace("${Born}", c_born, new_file)
-# to replace everything that you need in the template
+templates = ["test.docx", "Згода на обробку персональних даних.docx"]
+
+for template in templates:
+    file_name_date = contract_date[6:10] + contract_date[2:6] + contract_date[0:2]
+    new_file = file_name_date + "-" + str(template)
+    # to create the file name with revers position of date
+
+
+
+
+
+    text_replace("${Contract date}", date_by_word, template)
+    text_replace("${Name}", c_name, new_file)
+    text_replace("${Passport ID}", c_passport_id, new_file)
+    text_replace("${Passport address}", c_passport_address, new_file)
+    text_replace("${Passport date}", c_passport_date, new_file)
+    text_replace("${Address}", c_address, new_file)
+    text_replace("${FOP address}", c_fop_address, new_file)
+    text_replace("${ID}", c_id, new_file)
+    text_replace("${Bank}", c_bank, new_file)
+    text_replace("${Bank info}", c_bank_info, new_file)
+    text_replace("${Person bank info}", c_person_bank_info, new_file)
+    text_replace("${FOP ID}", c_fop_id, new_file)
+    text_replace("${FOP ID date}", c_fop_id_date, new_file)
+    text_replace("${Born}", c_born, new_file)
+    # to replace everything that you need in the template
---
+++ test.py
@@ -0,0 +1,4 @@
+numbers = [1, 2, 3, 4, 5]
+choose =  input()
+choose = int(choose)
+print(numbers[choose])
