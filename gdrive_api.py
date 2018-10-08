71a3c53487459902e0bf8adfaee97aac139f6269
Yura-D <dyachuk.yu@gmail.com>
2018-10-08 09:31:57 +0300

    made small changes in file

--- gdrive_api.py
+++ gdrive_api.py
@@ -6,22 +6,24 @@ from oauth2client import client, tools, file
 import io
 
 # If modifying these scopes, delete the file token.json.
+# Authorization to Google drive with personal token
 SCOPES = 'https://www.googleapis.com/auth/drive'
+store = file.Storage('token.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = build('drive', 'v3', http=creds.authorize(Http()))
 
-def main():
+def get_list(gfolder):
     """Shows basic usage of the Drive v3 API.
     Prints the names and ids of the first 10 files the user has access to.
     """
-    store = file.Storage('token.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-    service = build('drive', 'v3', http=creds.authorize(Http()))
+    
 
     # Call the Drive v3 API
     results = service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)", q="'1fGkO8AuLMJIN9gL-oVxWfZNS2rTQfMnp' in parents").execute() # you can change text 'test' "name contains 'test'"
+        fields="nextPageToken, files(id, name)", q= "'{0}' in parents".format(gfolder)).execute() # you can change text 'test' "name contains 'test'"
     items = results.get('files', [])
 
     if not items:
@@ -29,8 +31,7 @@ def main():
     else:
         print('Files:')
         for item in items:
-            print(item['id'])
-            print('{0} ({1})'.format(item['name'], item['id']))
+            print('* {0} - ({1})'.format(item['name'], item['id']))
 
     """
     # upload file
@@ -61,6 +62,4 @@ def main():
         fh.seek(0)
         f.write(fh.read())
     """
-
-if __name__ == '__main__':
-    main()
+# 1fGkO8AuLMJIN9gL-oVxWfZNS2rTQfMnp
